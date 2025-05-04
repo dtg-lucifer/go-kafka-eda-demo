66e9a488359b97746bf071627b0a417dcaa403e9
devpiush <dev.bosepiush@gmail.com>
2025-05-04 22:31:28 +0530

    code: working on the producer

---
+++ producer/producer.go
@@ -0,0 +1,50 @@
+package producer
+
+import "github.com/IBM/sarama"
+
+type ProducerDTO struct {
+	Topic string `json:"topic"`
+	Data  []byte `json:"data"`
+}
+
+func ConnectProducer(brokers []string) (sarama.SyncProducer, error) {
+	config := sarama.NewConfig()
+	config.Producer.Return.Successes = true
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
+	config.Producer.Compression = sarama.CompressionSnappy
+	config.Producer.Partitioner = sarama.NewRoundRobinPartitioner
+
+	conn, err := sarama.NewSyncProducer(brokers, config)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+func PushToQueue(dto ProducerDTO) error {
+	brokers := []string{"localhost:9092"}
+
+	// @INFO Open a connection to the Kafka cluster
+	conn, err := ConnectProducer(brokers)
+	if err != nil {
+		return err
+	}
+
+	// @INFO Close the connection when the function exits
+	defer func() {
+		if err := conn.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	_, _, err = conn.SendMessage(&sarama.ProducerMessage{
+		Topic: dto.Topic,
+		Value: sarama.ByteEncoder(dto.Data),
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
